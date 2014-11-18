2c02c267ac4e3e3456a5911f7f7c66cf35fce54e
yanglujuncc <yanglujuncc@gmail.com>
2014-11-18 09:45:28 +0800

    firstCommit

---
+++ src/main/java/ylj/webdata/App.java
@@ -0,0 +1,13 @@
+package ylj.webdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
